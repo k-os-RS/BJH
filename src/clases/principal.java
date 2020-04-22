d0b7b1d3dea082f854c6214e4d7c80b635e2b771
wSilent_ <moisesbrandon@gmail.com>
2020-04-22 22:42:05 +0200

    v1.0
    Carpetas y javas hechos

---
+++ src/clases/principal.java
@@ -0,0 +1,8 @@
+package clases;
+import ventanas.logeo;
+public class principal {
+	public static void main(String [] args) {
+		logeo iniciador = new logeo();
+		iniciador.iniciar();
+	}
+}
---
+++ src/gestion_db/metodos_db.java
@@ -0,0 +1,5 @@
+package gestion_db;
+
+public class metodos_db {
+
+}
